abb2d94a06ae3868a7322c7fec0aa5df4c5bb21e
Jonathan Ho <johnnyho82@gmail.com>
2019-09-05 17:26:29 -0400

    Add tests for rendering and text on DOM

--- baseball-scoreboard/src/components/Display.js
+++ baseball-scoreboard/src/components/Display.js
@@ -5,9 +5,38 @@ export default function Display(props) {
     <div>
       <div>
         <h2>Inning: 1</h2>
-        <h2>Strikes: {props.strikes}</h2>
-        <h2>Balls: {props.balls}</h2>
+        <h2>
+          Strikes: <span data-testid='strikes'>{props.strikes}</span>
+        </h2>
+        <h2 data-testid='balls'>Balls: {props.balls}</h2>
         <h2>Outs: {props.outs}</h2>
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+              <th>6</th>
+              <th>7</th>
+              <th>8</th>
+              <th>9</th>
+              <th>Runs</th>
+              <th>Errors</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Home</td>
+            </tr>
+            <tr>
+              <td>Away</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
