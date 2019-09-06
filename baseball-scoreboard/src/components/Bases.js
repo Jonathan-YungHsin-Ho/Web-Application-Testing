803949b558c8926e665f049977745afd79c75cc2
Jonathan Ho <johnnyho82@gmail.com>
2019-09-05 21:25:11 -0400

    Add runs scored functionality

--- baseball-scoreboard/src/components/Bases.js
+++ baseball-scoreboard/src/components/Bases.js
@@ -4,6 +4,7 @@ export default function Bases(props) {
   const [first, setFirst] = useState(false);
   const [second, setSecond] = useState(false);
   const [third, setThird] = useState(false);
+  const [runsScored, setRunsScored] = useState(0);
 
   const handleSingle = () => {
     setFirst(true);
@@ -17,6 +18,13 @@ export default function Bases(props) {
         return true;
       }
     });
+    setRunsScored(prevState => {
+      if (third) {
+        return prevState + 1;
+      } else {
+        return prevState;
+      }
+    });
   };
 
   const handleDouble = () => {
@@ -27,23 +35,61 @@ export default function Bases(props) {
         return true;
       }
     });
+    setRunsScored(prevState => {
+      let runs = 0;
+      if (third) {
+        runs++;
+      }
+      if (second) {
+        runs++;
+      }
+      return prevState + runs;
+    });
   };
 
   const handleTriple = () => {
     setFirst(false);
     setSecond(false);
     setThird(true);
+    setRunsScored(prevState => {
+      let runs = 0;
+      if (third) {
+        runs++;
+      }
+      if (second) {
+        runs++;
+      }
+      if (first) {
+        runs++;
+      }
+      return prevState + runs;
+    });
   };
 
   const handleHomeRun = () => {
     setFirst(false);
     setSecond(false);
     setThird(false);
+    setRunsScored(prevState => {
+      let runs = 1;
+      if (third) {
+        runs++;
+      }
+      if (second) {
+        runs++;
+      }
+      if (first) {
+        runs++;
+      }
+      return prevState + runs;
+    });
   };
+
+  const handleResetRuns = () => setRunsScored(0);
 
   return (
     <div className='field'>
-      <h2>Runs Scored: </h2>
+      <h2>Runs Scored: {runsScored}</h2>
       <div className='bases'>
         <h3>
           <div className='base'>{second ? '⚾' : '⬜'}</div>
@@ -62,6 +108,7 @@ export default function Bases(props) {
         <button onClick={handleDouble}>Double</button>
         <button onClick={handleTriple}>Triple</button>
         <button onClick={handleHomeRun}>Homerun</button>
+        <button onClick={handleResetRuns}>Reset Runs</button>
       </div>
     </div>
   );
